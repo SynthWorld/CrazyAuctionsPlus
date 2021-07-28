8fb41083449adc2b261f85a1d1d6ca0bc8ea220d
TRCStudioDean <50910791+TRCStudioDean@users.noreply.github.com>
2021-07-28 23:52:21 +0800

    Updated to 1.1.5

--- src/main/java/studio/trc/bukkit/crazyauctionsplus/database/storage/MySQLStorage.java
+++ src/main/java/studio/trc/bukkit/crazyauctionsplus/database/storage/MySQLStorage.java
@@ -9,8 +9,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
